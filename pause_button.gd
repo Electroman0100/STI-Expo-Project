823cefaf336cd8b7c7f134d8e98aa0a9c645511c
Pea-body <jakegotaya@gmail.com>
2025-04-28 10:32:08 +0800

    Add files via upload
    mapa ni

--- pause_button.gd
+++ pause_button.gd
@@ -1,9 +1,8 @@
-extends Control
+extends Button
 
 
 func resume():
 	get_tree().paused = false
-	$Control.hide()
 	
 func pause():
 	get_tree().pressed = true
@@ -13,12 +12,15 @@ func testEsc():
 		pause()
 	elif Input.is_action_just_pressed("toggle_pause") and get_tree().paused == true:
 		resume()
+		hide()
 
 
 
 func _on_resume_button_pressed() -> void:
-	resume()
-
+	var tree = get_tree()
+	tree.paused = false  # Unpause the game
+	hide()
+	
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
